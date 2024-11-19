0080979925eada8c0cfe16525bbd1b7f0ff6982d
Devansh Rajani <devanshrajani25@gmail.com>
2024-11-19 11:27:25 +0530

    set up first

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='MLProject',
+    version='0.0.1',
+    author='Devansh',
+    author_email='devanshrajani25@gmail.com',
+    packages=find_packages(),
+    requires=['numpy','pandas']
+)
